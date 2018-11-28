6e6aa8ce77dfa8eef84b29ab21b08530bee8f5c9
Doxing <joegou721@gmail.com>
2018-11-28 21:37:42 +0800

    update test2.py

--- test2.py
+++ test2.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+
 html = requests.get('http://www.taiwanlottery.com.tw/result_all.aspx')
 html.encoding = 'utf-8'
 sp = BeautifulSoup(html.text, 'html.parser')
 
-data1 = sp.find('h1', class_='font_red18')
-data2 = sp.find('table', class_='tableWin')
-data3 = data2.find_all('span')
-print(data1.text.strip()+'開獎順序:')
-for n in range(3,len(data3)):
-    print(data3[n].text+'',end='')
+#抓取最外層
+bs1 = sp.select('#right_full')
+
+#威力彩
+bs2 = sp.find('h1', class_=' font_red18')
+
+#表格
+bs3 = bs1[0].find('table',{'class':'tableWin'})
+bs4 = bs3.find_all('span')
+
+#打印出威力彩+開獎號碼,刪除後面空格
+print(bs2.text+'開獎號碼:', end='')
+
+#打印從第三個span到第九個span的文字內容,刪除後面空格
+for n in range(3,10):
+    print(bs4[n].text, end='')
+
+
